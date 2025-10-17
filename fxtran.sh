efcab47cd2dac5cb34f8d99fadf853a6fc40edd3
Erwan Cossevin <rma1@ac6-197.bullx>
2025-10-17 15:41:51 +0000

    add copy, wipe methods

--- fxtran.sh
+++ fxtran.sh
@@ -1,7 +1,7 @@
 export PATH=/home/sor/bin:/home/sor/bin/vimpack.d:/home/sor/fxtran/master/bin:$PATH
 modules_prefix=/perm/rma1/test_case_working_week/generate_util
-/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load -- f90 --dir tmp -c $modules_prefix/tmp/model_physics_mf_mod.F90
-/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load -- f90 --dir tmp -c $modules_prefix/tmp/yomphy0.F90
-/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load -- f90 --dir tmp -c $modules_prefix/tmp/yomphy.F90
-/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load -- f90 --dir tmp -c $modules_prefix/tmp/yomphy2.F90
-/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load -- f90 --dir tmp -c $modules_prefix/tmp/yomcst.F90
+/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load,copy,wipe -- f90 --dir tmp -c $modules_prefix/tmp/model_physics_mf_mod.F90
+/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load,copy,wipe -- f90 --dir tmp -c $modules_prefix/tmp/yomphy0.F90
+/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load,copy,wipe -- f90 --dir tmp -c $modules_prefix/tmp/yomphy.F90
+/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load,copy,wipe -- f90 --dir tmp -c $modules_prefix/tmp/yomphy2.F90
+/home/rma1/fxtran-acdc/bin/fxtran-f90 --dryrun --method methods --methods save,load,copy,wipe -- f90 --dir tmp -c $modules_prefix/tmp/yomcst.F90
